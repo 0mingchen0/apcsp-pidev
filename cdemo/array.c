c09c12b9bc431f167c55f34f50f8a61f52f8ed31
Mingchen <wangm9449@my.sduhsd.net>
2019-05-28 10:07:52 -0700

    added arrays, and it does not work lol

---
+++ cdemo/array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+int arr[100];
+for (int i = 0; i < 5; i++)
+{
+arr[i] = i * i;
+}
+printf("1st and 90th vals: %d, %d\n,also I cant get this woking yea ", arr[0], arr[90]);
+  return 0;
+}
