780fa198ab83f3bc16708931aefea6da3563c9bb
Mingchen <wangm9449@my.sduhsd.net>
2019-05-28 11:07:16 -0700

    made changes to student now it works

--- cdemo/student.c
+++ cdemo/student.c
@@ -1,7 +1,7 @@
 
-
 #include <stdio.h>
 #include <string.h>
+
 struct Student {
 char firstname[50];
 char lastname[50];
@@ -10,22 +10,23 @@ int id;
 };
 
 void printStudent();
+
 int main()
 {
-struct Student student;
+  struct Student student;
+  strcpy(student.firstname, "First");
+  strcpy(student.lastname, "Last");
+  student.age = 15;
+  student.id = 1234567;
 
-strcpy(student.firstname, "First");
-strcpy(student.lastname, "Last");
-student.age = 15;
-student.id = 1234567;
-printStudent();
+  printStudent(&student);
 }
 
 void printStudent(struct Student* student)
 {
 printf("student: \n");
 printf(" firstname:%s\n", student->firstname);
-printf(" age:%d\n", student->age);
 printf(" lastname:%s\n", student->lastname);
+printf(" age:%d\n", student->age);
 printf(" id:%d\n", student->id);
 }
